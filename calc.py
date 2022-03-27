2bbff9696d16235ae2c92b6612f300e6785e2c9b
zolmine <68522174+zolmine@users.noreply.github.com>
2022-03-27 20:57:50 +0000

    moving to the probabilities tester

---
+++ calc.py
@@ -0,0 +1,58 @@
+from scipy import optimize
+
+
+def u(Z, F, E, fee):
+    txFee = 10000 - fee
+    return (Z*txFee*F) / ((E*10000) + (Z*txFee))
+
+
+def z(Y, D, C, fee):
+    txFee = 10000 - fee
+    return (Y*txFee*D) / ((C*10000) + (Y*txFee))
+
+
+def y(X, B, A, fee):
+    txFee = 10000 - fee
+    return (X*txFee*B) / ((A*10000) + (X*txFee))
+
+
+def w1(X, A, B, fee1, C, D, fee2, verbose=True):
+    Y = y(X, B, A, fee1)
+    Z = z(Y, D, C, fee2)
+    W = Z - X
+    if verbose:
+        print("The value of y is", Y)
+        print("The value of z is", Z)
+        print("Value of w is", W)
+    return W
+
+
+def w2(X, A, B, fee1, C, D, fee2, E, F, fee3, verbose=True):
+    Y = y(X, B, A, fee1)
+    Z = z(Y, D, C, fee2)
+    U = u(Z, F, E, fee3)
+    W = U - X
+    if verbose:
+        print("The value of y is", Y)
+        print("The value of z is", Z)
+        print("The value of u is", U)
+        print("Value of w is", W)
+    return W
+
+
+def find_x_and_w(w_fn, verbose=True, **kwargs):
+    def f(x): return w_fn(X=x, **kwargs, verbose=False) * -1
+    x = optimize.minimize_scalar(f, bounds=(0, None)).x
+    w = w_fn(X=x, **kwargs, verbose=False)
+    if verbose:
+        print("The value of x is", x)
+        print("The value of w is", w)
+    return x, w
+
+
+# print("##### W1")
+# find_x_and_w(w1, A=11000, B=10000,fee1=12, C=10000, D=11500,fee2=24)
+
+print("\n##### W2")
+find_x_and_w(w2, A=11000, B=10000, fee1=5, C=10000,
+             D=9000, fee2=14, E=9200, F=12500, fee3=26)
--- newIndex.py
+++ newIndex.py
@@ -5,8 +5,8 @@ def getReserve(allPairs):
     allData = []
     # ws = create_connection("wss://speedy-nodes-nyc.moralis.io/02799b1f72329a0eefa3b741/polygon/mainnet/ws")
     for one in allPairs:
-        print(one) 
-        allData.append({"jsonrpc":"2.0","method":"eth_call","params":[{"to": one["pair"], "data":"0x0902f1ac"}, "latest"],"id":one["pair"][0:10]})
+        # print(one) 
+        allData.append({"jsonrpc":"2.0","method":"eth_call","params":[{"to": one, "data":"0x0902f1ac"}, "latest"],"id":one[0:10]})
     return allData
 
 
@@ -32,6 +32,7 @@ def functionOf3(place,baseAddr,tokenAddr,dex):
 # getting the secondBaseAddr and the thirdPair----------------------------------------------------------------------------------------------------------
     
     # thirdPair ----------------------------------------------------
+        i = 1
     for item in listWithoutMainPair:
         secondPair = item["pair"]
         secondPairFee = item["fee"]
@@ -42,32 +43,48 @@ def functionOf3(place,baseAddr,tokenAddr,dex):
             secondBasePlace = 1
             secondBaseAddr = item["token0"]
 
-            for elem in allPairs:
-                if (elem["token0"] == secondBaseAddr or elem["token0"] == baseAddr) and (elem["token1"] == secondBaseAddr or elem["token1"] == baseAddr):
-                    if elem["token0"] == secondBaseAddr:
-                        thirdBasePlace = 0
-                    elif elem["token1"] == secondBaseAddr:
+        for elem in allPairs:
+            if (elem["token0"] == secondBaseAddr or elem["token0"] == baseAddr) and (elem["token1"] == secondBaseAddr or elem["token1"] == baseAddr):
+                if elem["token0"] == secondBaseAddr:
+                    thirdBasePlace = 0
+                elif elem["token1"] == secondBaseAddr:
+                    thirdBasePlace = 1
+                thirdPaireFee = elem["fee"]
+                totalPairList.append(secondPair)
+                totalPairList.append(elem['pair'])
+                
+                if place == "first":
+                    results.append({"id":i,"firstPair":mainPair,"firstBase":firstBasePlace,"firstFee":mainPairfee,"secondPair":secondPair,"secondBase":secondBasePlace,"secondFee":secondPairFee,"thirdPair":elem['pair'],"thirdBase":thirdBasePlace,"thirdFee":thirdPaireFee})
+                elif place == "last":
+                    if firstBasePlace == 0:
+                        firstBasePlace = 1
+                    else:
+                        firstBasePlace = 0
+                    
+                    if thirdBasePlace == 0:
                         thirdBasePlace = 1
-                    thirdPaireFee = elem["fee"]
-                    totalPairList.append(secondPair)
-                    totalPairList.append(elem['pair'])
-    # print(totalPairList)
-                    # print(f"{mainPair},{firstBasePlace},{secondPair},{secondBasePlace},{elem['pair']},{thirdBasePlace}")
-                    results.append({"firstPair":mainPair,"firstBase":firstBasePlace,"firstFee":mainPairfee,"secondPair":secondPair,"secondBase":secondBasePlace,"secondFee":secondPairFee,"thirdPair":elem['pair'],"thirdBase":thirdBasePlace,"thirdFee":thirdPaireFee})
+                    else:
+                        thirdBasePlace = 0
+
+                    results.append({"id":i,"firstPair":elem['pair'],"firstBase":thirdBasePlace,"firstFee":thirdPaireFee,"secondPair":secondPair,"secondBase":secondBasePlace,"secondFee":secondPairFee,"thirdPair":mainPair,"thirdBase":firstBasePlace,"thirdFee":mainPairfee})
+                i = i + 1
+                        # last == decroissant order and reverse bases
     totalPairList = list(dict.fromkeys(totalPairList))
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
-    print(results,totalPairList)
+    request = getReserve(totalPairList)
+    returnedResult = {
+        "request": request,
+        "probabilities":results
+    }
+    print(returnedResult)
+    return(returnedResult)
     # print("------------------------------------------------------------------------------------------------------------------------------")
 # functionOf3("first","0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270","0x172370d5Cd63279eFa6d502DAB29171933a610AF","1")
-
-
-                        
-
-
 
 def funcFor2(place,baseAddr,tokenAddr,dex):
     result = []
     newList = []
+    filtredPairList = []
     for one in allPairs:
         if one["dex"] == dex and (one["token0"] == baseAddr or one["token0"] == tokenAddr) and (one["token1"] == baseAddr or one["token1"] == tokenAddr):
             mainPair = one["pair"]
@@ -84,40 +101,58 @@ def funcFor2(place,baseAddr,tokenAddr,dex):
                     tokenAddrPlace = 1
                 secondPairFee = one["fee"]
                 newList.append({"pair":one['pair'],"base":tokenAddrPlace,"fee":secondPairFee})
-    
+    i = 1
     for item in newList:
-        result.append({"firstPair":mainPair,"firstBase":basePlace,"firstFee":mainPairFee,"secondPair":item['pair'],"secondBase":item['base'],"secondFee":item["fee"]})
-    newList.append({"pair":mainPair,"base":basePlace,"fee":mainPairFee})
-    request = getReserve(newList)
-    print(request)
-    return(result,request)
+        
+        if place == "first":
+            result.append({"id":i,"firstPair":mainPair,"firstBase":basePlace,"firstFee":mainPairFee,"secondPair":item['pair'],"secondBase":item['base'],"secondFee":item["fee"]})
+        elif place == "last":
+            if basePlace == 0:
+                secondBase = 1
+            else:
+                secondBase = 0
+
+            if item['base'] == 0:
+                firstBase = 1
+            else:
+                firstBase = 0
+            result.append({"id":i,"firstPair":item['pair'],"firstBase":firstBase,"firstFee":item["fee"],"secondPair":mainPair,"secondBase":secondBase,"secondFee":mainPairFee})
+        i = i + 1
+        filtredPairList.append(item['pair'])
+        
+    filtredPairList.append(mainPair)
+    request = getReserve(filtredPairList)
+    returnedResult = {
+        "request": request,
+        "probabilities":result
+    }
+    # print(request)
+    print(returnedResult)
+    return(returnedResult)
 
 
 funcFor2("first","0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270","0x172370d5Cd63279eFa6d502DAB29171933a610AF","1")
 
 
 
+def decodeReserve(input):
+    rererve0 = int(input[2:66],16)
+    rererve1 = int(input[66:130],16)
+    return(rererve0,rererve1)
 
-    # ws.send(json_data)
-    
-    # results = ws.recv()
-    # if results:
-    #     results = loads(results)
-    # else:
-    #     results = ws.recv()
-    #     results = loads(results)
-    
-    # for item in allPairs:
-    #     for sOne in results:
-    #         if item["pair"][0:10] == sOne["id"]:
-    #             reserve = decodeReserve(sOne["result"])
-    #             if item["base"] == 0:
-    #                 reserve0 = reserve[0]
-    #                 reserve1 = reserve[1]
-    #             elif item["base"] == 1:
-    #                 reserve0 = reserve[1]
-    #                 reserve1 = reserve[0]
-    #             print(item, reserve0,reserve1)
+
+def response(results):
+    for item in allPairs:
+        for sOne in results:
+            if item["pair"][0:10] == sOne["id"]:
+                reserve = decodeReserve(sOne["result"])
+                if item["base"] == 0:
+                    reserve0 = reserve[0]
+                    reserve1 = reserve[1]
+                elif item["base"] == 1:
+                    reserve0 = reserve[1]
+                    reserve1 = reserve[0]
+                print(item, reserve0,reserve1)
 
 
 # def functionOf4(place,baseAddr,tokenAddr,dex):
