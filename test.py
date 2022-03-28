2b415da7feb07aeb5dd8ae5238854dd98b40e9c6
zolmine <68522174+zolmine@users.noreply.github.com>
2022-03-28 11:02:32 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
 
 import math
-var = 1461501637330902918203684832716283019655932542975
+var = 1461501637330902918203684832716283019655879325429757
 
-# x = 1461501637330902918203684832716283019655932542975/
+var2 = 12089258196146291747061760
+# n = 11
+ 
+ 
+def Next(n):
+     
+    # Get the ceiling of
+    # exact square root of n
+    ceil = 1 + math.isqrt(n)
+    print(ceil)
 
-for i in range(100000000):
-    y = math.sqrt(var)
-    result = y * y
-    if var == result:
-        print(True,y)
-        break
-    var = var - i
-
-    # if type(y) == int:
-    #         print("int")
-    #         print(i)
-    #         print(y)
-    # x = x - i
-
-    # print(int(y))
-
-    # if y * y == x:
-    
+    # print the next perfect square of n
+    print("Next perfect square of {} is {}".
+          format(n, ceil*ceil))
+    print("missingNumber",(ceil*ceil) - var)
+    print(hex(ceil),hex((ceil*ceil) - var))
+    # return ceil
+    print(len(hex(ceil)))
+ 
+ 
+# Driver's code
+Next(var)
+# next(var2)
+# Next(37)
+# var = math.isqrt(146150163733090291820368483271628301965593254297600)
+# print(math.isqrt(146150163733090291820368483271628301965593254297600))
+# print(var*var)
 
 
+
+
+# math.isqrt(146150163733090291820368483271628301965593254297600)
